771cfe6b09877f9d33556c601d55d74ac8c0ee90
manh <luongvanmanh1929@gmail.com>
2022-07-06 17:45:44 +0700

    update next page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,13 @@
 $(document).ready(function () {
-   
+    function toggle_visibility(el){
+        var menu = document.getElementsByClassName("container");
+        for (i = 0; i < menu.length; i++) {
+            menu[i].style.display = "none";
+        }
+        document.getElementById(el).style.display = "block";
+    }
+    $(".menu-link").on("click", function () {
+        var atribute = $(this).attr("title");
+        toggle_visibility(atribute);
+    });
 });
